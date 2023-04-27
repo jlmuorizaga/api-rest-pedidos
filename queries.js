ceb26618d4d00ff164271c7dadbb1fa5f2349209
JOSE LUIS MU ORIZAGA <jlmuorizaga@gmail.com>
2023-04-26 23:33:10 -0600

    Actualizar rutas en queries.js

--- queries.js
+++ queries.js
@@ -1,17 +1,17 @@
 const Pool = require('pg').Pool
 
-/*const DB_HOST = process.env.DB_HOST || 'chp202304db.c2fc27t7knic.us-east-2.rds.amazonaws.com'
+const DB_HOST = process.env.DB_HOST || 'chp202304db.c2fc27t7knic.us-east-2.rds.amazonaws.com'
 const DB_USER = process.env.DB_USER || 'cheesepizzauser'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'cheesepizza2001'
 const DB_NAME = process.env.DB_NAME || 'cheesepizzapedidosmovilesdb'
 const DB_PORT = process.env.DB_PORT || 5432
-*/
-const DB_HOST = process.env.DB_HOST || 'localhost'
+
+/*const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'cheesepizzauser'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'cheesepizza2001'
 const DB_NAME = process.env.DB_NAME || 'cheesepizzapedidosmovilesdb'
 const DB_PORT = process.env.DB_PORT || 5434
-
+*/
 //Pool de conexiones a base de datos
 const pool = new Pool({
     user: DB_USER,
