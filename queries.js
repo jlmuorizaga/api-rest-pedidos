19b99a1103eeab2a9a2101217d21abef8c5a4a67
moondragon-factoria <124836257+moondragon-factoria@users.noreply.github.com>
2023-05-03 18:25:58 -0600

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,19 +1,21 @@
 const Pool = require('pg').Pool
 
 //Base de datos local JLMG
+/*
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'userchpmovildb'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'Secreto'
 const DB_NAME = process.env.DB_NAME || 'chppedidosmovildb'
 const DB_PORT = process.env.DB_PORT || 5432
+*/
 
-/*
+//Base de datos en AWS
 const DB_HOST = process.env.DB_HOST || 'chp202304db.c2fc27t7knic.us-east-2.rds.amazonaws.com'
 const DB_USER = process.env.DB_USER || 'cheesepizzauser'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'cheesepizza2001'
 const DB_NAME = process.env.DB_NAME || 'cheesepizzapedidosmovilesdb'
 const DB_PORT = process.env.DB_PORT || 5432
-*/
+
 
 /*const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'cheesepizzauser'
