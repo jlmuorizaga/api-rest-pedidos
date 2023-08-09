81b8822c18ec3364acee1622cac78b56c42c6339
moondragon-factoria <124836257+moondragon-factoria@users.noreply.github.com>
2023-08-08 23:07:15 -0600

    Nuevo index.js con nombres de endpoints ajustados

--- index.js
+++ index.js
@@ -7,37 +7,34 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
-    response.json({ info: 'API CHPSystem Pedidos Móviles Nube ver: 20230520 1350' })
-})
+    response.json({ info: 'API CHPSystem Pedidos Móviles Nube ver: 20230808 2055' });
+});
+
+//Endpoints
 
 //Endpoints para app móvil
-//Endpoints de pedidos
-app.post("/datos", db.insertaDatosPedido);
-//app.post('/pedido', db.createPedido);
-app.get('/estatusPedido/:id_pedido', db.getEstatusPedido);
-app.get('/pedidosCliente/:id_cliente', db.getPedidosByCliente);
+
+//Endpoints de pedido
+app.post('/pedido', db.insertaPedido);
+app.put('/pedido/:id_pedido', db.actualizaPedido);
+app.get('/pedido/:id_pedido', db.getPedidoById);
+app.get('/pedido/cliente/:id_cliente', db.getPedidosByCliente);
+app.get('/pedido/sucursal/:clave_sucursal', db.getPedidosBySucursal);
+app.put('/pedido/estatus/:estatus/:id_pedido', db.updateEstatusPedido);
 
 //Endpoints de clientes
-app.get('/accesoCliente/:correo/:contrasenia', db.getClienteAcceso);
-app.get('/accesoCliente/:correo', db.getDatosCliente);
-app.post('/accesoCliente', db.insertaCliente);
-app.put('/accesoCliente', db.actualizaCliente);
-
-//Endpoints para receptor de pedidos en sucursal
-app.get('/pedidos/:clave_sucursal', db.getPedidosBySucursal);
-app.get('/pedido/:id_pedido', db.getPedidoById);
-app.put('/pedidoRecibido/:id_pedido', db.updateEstatusRecibido);
-app.put('/pedidoCapturado/:id_pedido', db.updateEstatusCapturado);
-app.put('/pedidoEnviado/:id_pedido', db.updateEstatusEnviado);
-app.put('/pedidoListo/:id_pedido', db.updateEstatusListo);
-app.put('/pedidoAtendido/:id_pedido', db.updateEstatusAtendido);
+app.get('/cliente/acceso/:correo/:contrasenia', db.getClienteAcceso);
+app.get('/cliente/:correo', db.getDatosCliente);
+app.post('/cliente', db.insertaCliente);
+app.put('/cliente', db.actualizaCliente);
+app.get('/lugar', db.getLugares);
 
 //Endpoints de utilería sólo para pruebas de desarrollo
 app.put('/pedidosReset', db.updateEstatusPedidosReset);
 
 app.listen(port, () => {
-    console.log('App corriendo en puerto',port);
-})
+    console.log('API CHPSystem Pedidos Móviles Nube corriendo en puerto',port);
+});
