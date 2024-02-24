2841e44b15eb13304dbf08b8e08997871ce0596c
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2024-02-24 12:32:33 -0600

    Update queries_catalogos.js

--- queries_catalogos.js
+++ queries_catalogos.js
@@ -100,7 +100,7 @@ const getProductosBySucursal = (request, response) => {
 const getTipoProductosBySucursal = (request, response) => {
     const cve_sucursal = request.params.cve_sucursal
     pool.query(
-        'SELECT distinct pt.id,pt.descripcion,pt.img_url as imgUrl '
+        'SELECT distinct pt.id,pt.descripcion,pt.img_url as "imgUrl" '
         +'FROM preesppropro.relacion_producto_sucursal as rps, '
         +'preesppropro.sucursal as s,'
         +'preesppropro.producto as p,'
