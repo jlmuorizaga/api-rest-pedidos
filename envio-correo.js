471f36d930be38bcb1d093cd402cd1854669b9b3
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2024-03-23 14:06:05 -0600

    Correo de Verificación

--- envio-correo.js
+++ envio-correo.js
@@ -12,22 +12,45 @@ const transporter = nodeMailer.createTransport({
     }
 })
 
-let mail={
-    //from:"jlmuorizaga@gmail.com",
-    //from:"chp01ame@gmail.com",
-    to:"jlmuo@yahoo.com",
-    subject:"Hola esta es una prueba",
-    text:"Hola esta es una prueba de correo",
-    html:`<h1 style="color:red;">Este mensaje fue enviado usando nodemailer</h1>`
+
+
+
+
+
+const verificaCorreo = (req, res) => {
+    const { correo,asunto,codigoVerificacion } = req.body;
+    let mail={
+        //from:"jlmuorizaga@gmail.com",
+        //from:"chp01ame@gmail.com",
+        to:correo,
+        subject:asunto,
+        //text:"Hola esta es una prueba de correo",
+        html:'<h3>Sistema CheesePizza de Pedidos Móviles</h3><p>Su código de verificación es: </p>'+
+        '<h1>'+codigoVerificacion+'</h1>'+
+        '<p>Su código será válido durante 10 minutos</p><br>'+
+        '<p><i>Esta es un correo automático, favor de no responder</i></p>'
+    }
+
+    transporter.sendMail(mail,(error,info)=>{
+        if (error) {
+
+            textoRespuesta = '{"respuesta": "Error al enviar correo a '+correo+'"}';
+            res.status(422).json(JSON.parse(textoRespuesta));
+            console.error("Error enviando correo: ",error);
+        }
+        else{
+            textoRespuesta = '{"respuesta": "Se ha enviado un correo a '+correo+'"}';
+            res.status(201).json(JSON.parse(textoRespuesta));
+            console.log("Correo enviado ",info);
+            //console.log("Correo enviado ");
+    
+        }
+    })
+
+
+
 }
 
-transporter.sendMail(mail,(error,info)=>{
-    if (error) {
-        console.error("Error enviando correo: ",error);
-    }
-    else{
-        //console.log("Correo enviado ",info);
-        console.log("Correo enviado ");
-
-    }
-})
+module.exports = {
+    verificaCorreo
+}
---
+++ index_correo.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const correo = require('./envio-correo');
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3003;
+
+app.post('/verifica-correo', correo.verificaCorreo);
+
+app.listen(port, () => {
+    console.log('API CHPSystem Servidor de Correos corriendo en puerto', port);
+});
