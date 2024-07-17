82c49dca1a4b48170aab913da5f9860492ebc89b
moondragon-factoria <124836257+moondragon-factoria@users.noreply.github.com>
2024-07-16 21:36:20 -0600

    categoria y subcategoria de pizzas

--- queries_catalogos.js
+++ queries_catalogos.js
@@ -63,8 +63,9 @@ const getTamaniosBySucursal = (request, response) => {
     const cve_sucursal = request.params.cve_sucursal
     const id_especialidad = request.params.id_especialidad
     pool.query(
-        'SELECT id_especialidad_pizza as "idEspecialidadPizza",r.id_tamanio_pizza as "idTamanioPizza",t.nombre,r.precio,r.aplica_2x1 as "aplica2x1", ' 
-        + 'r.aplica_p1 as "aplicaP1", r.precio_p1 as "precioP1", r.aplica_bebida_chica_gratis as "aplicaBebidaChicaGratis" '
+        'SELECT id_especialidad_pizza as "idEspecialidadPizza",r.id_tamanio_pizza as "idTamanioPizza",'
+        +'t.nombre,r.precio,r.aplica_2x1 as "aplica2x1", r.categoria, r.subcategoria, ' 
+        +'r.aplica_p1 as "aplicaP1", r.precio_p1 as "precioP1", r.aplica_bebida_chica_gratis as "aplicaBebidaChicaGratis" '
         +'FROM preesppropro.relacion_especialidad_tamanio_precio_sucursal AS r,'
         +'preesppropro.sucursal as s, preesppropro.tamanio_pizza as t '
         +'WHERE s.id=r.id_sucursal and r.id_tamanio_pizza=t.id ' 
